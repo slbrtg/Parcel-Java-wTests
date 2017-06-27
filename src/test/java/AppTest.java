e7e9334378d66ba8ea0c3d61d27edfce885d160e
Saul Artiga <saul@>
2017-06-27 15:17:47 -0700

    add test for App.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,12 +1,15 @@
-// import org.junit.*;
-// import static org.junit.Assert.*;
-// import java.util.List;
-// import java.util.ArrayList;
-//
-// public class AppTest {
-//
-//   @Test
-//   public void collectUserParcelInfo(){
-//
-//   }
-// }
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.*;
+
+public class AppTest {
+
+  @Test
+  public void testMain(){
+    String userParcelInfo = "3 3 3";
+    String[] parcelDimensions = userParcelInfo.split("\\s+", -1);
+    Parcel userParcel = new Parcel(Integer.parseInt(parcelDimensions[0]),Integer.parseInt(parcelDimensions[1]),Integer.parseInt(parcelDimensions[2]));
+    int expectedOutput = 9;
+    assertEquals(expectedOutput, userParcel.parcelCost());
+  }
+}
